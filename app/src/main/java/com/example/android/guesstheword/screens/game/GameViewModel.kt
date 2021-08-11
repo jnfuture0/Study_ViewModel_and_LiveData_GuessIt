7bc38f13ab25dae13fb17d2f474737bbb5a28bf0
TravelWallet <travelwallet@TravelWalletui-MacBookPro.local>
2021-08-11 16:24:12 +0900

    study ViewModel and LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,5 +16,130 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
 // TODO (02) Create the GameViewModel class, extending ViewModel
 // TODO (03) Add init and override onCleared; Add log statements to both
+
+class GameViewModel : ViewModel(){
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word:LiveData<String>
+        get() = _word
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+        get() = _score
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish :LiveData<Boolean>
+        get() = _eventGameFinish
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime : LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime){ time->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 10000L
+    }
+
+    private val timer:CountDownTimer
+
+    init{
+        resetList()
+        nextWord()
+        _eventGameFinish.value = false
+        _score.value = 0
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventGameFinish.value = true
+            }
+        }
+
+        timer.start()
+    }
+
+    fun onSkip() {
+        _score.value = (_score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (_score.value)?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinishComplete(){
+        _eventGameFinish.value = false
+    }
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //_eventGameFinish.value = true
+            resetList()
+        }
+        _word.value = wordList.removeAt(0)
+
+    }
+
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
+}
